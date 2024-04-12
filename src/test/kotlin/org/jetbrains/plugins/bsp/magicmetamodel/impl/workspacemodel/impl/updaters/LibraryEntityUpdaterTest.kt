d86a1dea751fe7c688ea6ce74e992e842386c3c2
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2024-04-12 17:49:27 +0200

    [fix] Add correct entity source for libraries + enforce project-level for all libraries | #BAZEL-931 Done

--- src/test/kotlin/org/jetbrains/plugins/bsp/magicmetamodel/impl/workspacemodel/impl/updaters/LibraryEntityUpdaterTest.kt
+++ src/test/kotlin/org/jetbrains/plugins/bsp/magicmetamodel/impl/workspacemodel/impl/updaters/LibraryEntityUpdaterTest.kt
@@ -4,19 +4,18 @@ import com.intellij.platform.workspace.jps.entities.LibraryEntity
 import com.intellij.platform.workspace.jps.entities.LibraryRoot
 import com.intellij.platform.workspace.jps.entities.LibraryRootTypeId
 import com.intellij.platform.workspace.jps.entities.LibraryTableId
-import com.intellij.platform.workspace.jps.entities.ModuleId
 import com.intellij.workspaceModel.ide.impl.LegacyBridgeJpsEntitySourceFactory
 import org.jetbrains.plugins.bsp.magicmetamodel.impl.workspacemodel.Library
 import org.jetbrains.workspace.model.matchers.entries.ExpectedLibraryEntity
 import org.jetbrains.workspace.model.matchers.entries.shouldBeEqual
 import org.jetbrains.workspace.model.matchers.entries.shouldContainExactlyInAnyOrder
-import org.jetbrains.workspace.model.test.framework.WorkspaceModelWithParentJavaModuleBaseTest
+import org.jetbrains.workspace.model.test.framework.WorkspaceModelBaseTest
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
-@DisplayName("LibraryEntityUpdater.addEntity(entityToAdd, parentModuleEntity) tests")
-internal class LibraryEntityUpdaterTest : WorkspaceModelWithParentJavaModuleBaseTest() {
+@DisplayName("LibraryEntityUpdater.addEntity(entityToAdd) tests")
+internal class LibraryEntityUpdaterTest : WorkspaceModelBaseTest() {
   private lateinit var libraryEntityUpdater: LibraryEntityUpdater
   private lateinit var workspaceModelEntityUpdaterConfig: WorkspaceModelEntityUpdaterConfig
 
@@ -41,7 +40,7 @@ internal class LibraryEntityUpdaterTest : WorkspaceModelWithParentJavaModuleBase
 
     // when
     val returnedLibraryEntity = runTestWriteAction {
-      libraryEntityUpdater.addEntity(library, parentModuleEntity)
+      libraryEntityUpdater.addEntity(library)
     }
 
     // then
@@ -55,10 +54,13 @@ internal class LibraryEntityUpdaterTest : WorkspaceModelWithParentJavaModuleBase
     )
     val expectedLibraryEntity = ExpectedLibraryEntity(
       libraryEntity = LibraryEntity(
-        tableId = LibraryTableId.ModuleLibraryTableId(ModuleId(parentModuleEntity.name)),
+        tableId = LibraryTableId.ProjectLibraryTableId,
         name = "BSP: file:///dependency/test/1.0.0/test-1.0.0.jar",
         roots = listOf(expectedLibrarySourcesRoot, expectedLibraryClassesRoot),
-        entitySource = parentModuleEntity.entitySource,
+        entitySource = LegacyBridgeJpsEntitySourceFactory.createEntitySourceForProjectLibrary(
+          workspaceModelEntityUpdaterConfig.project,
+          null
+        ),
       ),
     )
 
@@ -85,7 +87,7 @@ internal class LibraryEntityUpdaterTest : WorkspaceModelWithParentJavaModuleBase
 
     // when
     val returnedLibraryEntries = runTestWriteAction {
-      libraryEntityUpdater.addEntries(libraries, parentModuleEntity)
+      libraryEntityUpdater.addEntries(libraries)
     }
 
     // then
@@ -99,10 +101,13 @@ internal class LibraryEntityUpdaterTest : WorkspaceModelWithParentJavaModuleBase
     )
     val expectedLibraryEntity1 = ExpectedLibraryEntity(
       libraryEntity = LibraryEntity(
-        tableId = LibraryTableId.ModuleLibraryTableId(ModuleId(parentModuleEntity.name)),
+        tableId = LibraryTableId.ProjectLibraryTableId,
         name = "BSP: file:///dependency/test/1.0.0/test-1.0.0.jar",
         roots = listOf(expectedLibrarySourcesRoot1, expectedLibraryClassesRoot1),
-        entitySource = parentModuleEntity.entitySource,
+        entitySource = LegacyBridgeJpsEntitySourceFactory.createEntitySourceForProjectLibrary(
+          workspaceModelEntityUpdaterConfig.project,
+          null
+        ),
       ),
     )
 
@@ -116,10 +121,13 @@ internal class LibraryEntityUpdaterTest : WorkspaceModelWithParentJavaModuleBase
     )
     val expectedLibraryEntity2 = ExpectedLibraryEntity(
       libraryEntity = LibraryEntity(
-        tableId = LibraryTableId.ModuleLibraryTableId(ModuleId(parentModuleEntity.name)),
+        tableId = LibraryTableId.ProjectLibraryTableId,
         name = "BSP: file:///dependency/test2/2.0.0/test2-2.0.0.jar",
         roots = listOf(expectedLibrarySourcesRoot2, expectedLibraryClassesRoot2),
-        entitySource = parentModuleEntity.entitySource,
+        entitySource = LegacyBridgeJpsEntitySourceFactory.createEntitySourceForProjectLibrary(
+          workspaceModelEntityUpdaterConfig.project,
+          null
+        ),
       ),
     )
 
